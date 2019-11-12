2a7e83b67f2417f9704e7218e6e646ec93c4bc40
yejingxuan <yejingxuan@megvii.com>
2019-11-12 16:02:15 +0800

    集成docker

--- yjx-demo-docker/src/main/java/com/yjx/demo/docker/DockerApplication.java
+++ yjx-demo-docker/src/main/java/com/yjx/demo/docker/DockerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class YjxDemoDockerApplication {
+public class DockerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(YjxDemoDockerApplication.class, args);
+        SpringApplication.run(DockerApplication.class, args);
     }
 
 }
